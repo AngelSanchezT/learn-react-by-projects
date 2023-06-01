de46849e8d95f3a461be62537a79b5f0afbc0201
Angel Javier Sanchez Tenjo <ajsanchez22@gmail.com>
2023-05-31 23:38:58 -0500

    Component CounterApp

--- counter-app/src/main.jsx
+++ counter-app/src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CounterApp } from './CounterApp';
 
-// import { HelloWorldApp } from './HelloWorldApp';
 import { FirstApp } from './FirstApp';
+// import { HelloWorldApp } from './HelloWorldApp';
+import './styles.css';
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-
-        <FirstApp />
+        <CounterApp value={ 20 } />
+        {/* <FirstApp title="Hola, Soy Vegeta" /> */}
     </React.StrictMode>
 );
+
